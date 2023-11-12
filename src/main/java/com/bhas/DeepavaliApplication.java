a9a87943365beed91ddf0b96405f3f4e202632f5
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-12 23:55:00 +0530

    initial commit on Auspicious day of Deepavali

---
+++ src/main/java/com/bhas/DeepavaliApplication.java
@@ -0,0 +1,13 @@
+package com.bhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepavaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepavaliApplication.class, args);
+	}
+
+}
