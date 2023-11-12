293ee9269a08b8f4cfffca58689b362b8ee77134
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-12 23:56:57 +0530

    auspicious day wishes

--- src/main/java/com/bhas/DeepavaliApplication.java
+++ src/main/java/com/bhas/DeepavaliApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeepavaliApplication {
+public class DeepavaliApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(DeepavaliApplication.class, args);
+		System.out.println("\n \"victory of light over darkness, good over evil, and knowledge over ignorance");
 	}
 
 }
