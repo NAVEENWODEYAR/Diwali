3e24414449ba93eac774b140a722c98f832159e3
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-13 22:43:27 +0530

    Entity class added

--- src/main/java/com/bhas/DeepavaliApplication.java
+++ src/main/java/com/bhas/DeepavaliApplication.java
@@ -10,7 +10,7 @@ public class DeepavaliApplication
 	public static void main(String[] args)
 	{
 		SpringApplication.run(DeepavaliApplication.class, args);
-		System.out.println("\n \"victory of light over darkness, good over evil, and Knowledge over ignorance");
+		System.out.println("\n \"Victory of light over darkness, good over evil, and Knowledge over ignorance");
 	}
 
 }
